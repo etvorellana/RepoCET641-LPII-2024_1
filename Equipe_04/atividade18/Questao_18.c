2bea8294e94461488bbe9f0bfd8bc5da22269243
Antonio-HOS <ahosantos.cic@uesc.br>
2024-04-24 10:43:51 -0300

    questao 18 mudada

--- Equipe_04/atividade18/Questao_18.c
+++ Equipe_04/atividade18/Questao_18.c
@@ -1,35 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../lista_01.h"
 
-
-int quantidade(char str[], int inicio, int fim);
+int quantidade(char str[]);
 
 int main(){
-    char str_1[100];
+    char str_1[] = "StellA dA Silva Ribas";
 
-    printf("Digite uma string: ");
-    gets(str_1);
-
-    int tamanho = strlen(str_1);
-
-    int quant = quantidade(str_1, 0, tamanho - 1);
+    int quant = quantidade(str_1);
     printf("A quantidade eh: %d\n", quant);
 
     return 0;
 }
 
-int quantidade(char str[], int inicio, int fim) {
+int quantidade(char str[]) {
     int cont = 0;
 
-    if (inicio > fim){ //Caso base: se inicio > fim ent�o para de contar.
+    if (str[0] == '\0'){
         return 0;
     }
 
-    if (str[inicio] == 'A' || str[inicio] == 'a'){
-        cont = cont + 1;
+    else{
+        if(str[0] == 'A' || str[0] == 'a'){
+            cont = cont + 1;
+        }
+
+        cont+=quantidade(str + 1);
     }
 
-    return cont + quantidade(str, inicio + 1, fim);
+    return cont;
 }
