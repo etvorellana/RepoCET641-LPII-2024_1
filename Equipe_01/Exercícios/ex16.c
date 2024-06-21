f11cca02f6173c4a65044687889eea82e29a22a9
Arthur Araújo <142846540+artsarchive@users.noreply.github.com>
2024-06-21 18:56:14 -0300

    Update and rename Equipe_01/ex16.c to Equipe_01/Exercícios/ex16.c

--- Equipe_01/Exercícios/ex16.c
+++ Equipe_01/Exercícios/ex16.c
@@ -5,15 +5,15 @@
 int main() {
     char str[100];
 
-    // Solicita a entrada da string ao usu·rio
+    // Solicita a entrada da string ao usu√°rio
     printf("Digite uma string: ");
     scanf("%s", str);
 
-    // Verifica se a string È um palÌndromo
+    // Verifica se a string √© um pal√≠ndromo
     if (ehPalindromo(str))
-        printf("%s È um palÌndromo.\n", str);
+        printf("%s √© um pal√≠ndromo.\n", str);
     else
-        printf("%s n„o È um palÌndromo.\n", str);
+        printf("%s n√£o √© um pal√≠ndromo.\n", str);
 
     return 0;
 }
